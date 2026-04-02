11c3670b3059e5c8abd66966563c71198ddc2376
Pradeep Singh <pradeep.singh@devtechnosys.info>
2026-04-02 10:30:05 +0530

    changes

--- resources/views/mybooking/listing.blade.php
+++ resources/views/mybooking/listing.blade.php
@@ -1,3 +1,8 @@
 <x-frontend.main-layout>
-     <livewire:frontend.Mybooking.listing/>
+         @php
+        // Blade tak request aa rahi hai ya nahi
+        \Log::info('Blade file loaded for /my-booking route');
+        dd('Blade loaded'); // test
+    @endphp
+     <livewire:frontend.mybooking.listing/>
 </x-frontend.main-layout>
