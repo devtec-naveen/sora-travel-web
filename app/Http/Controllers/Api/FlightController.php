138fbba0d708e572bbc2bc2bfc3d53694e05b44f
Pradeep Singh <pradeep.singh@devtechnosys.info>
2026-04-09 11:40:21 +0530

    validation changes

--- app/Http/Controllers/Api/FlightController.php
+++ app/Http/Controllers/Api/FlightController.php
@@ -182,7 +182,7 @@ class FlightController extends Controller
         try {
             $validator = Validator::make($request->all(), [
                 'payment_intent_id'                                          => ['required', 'string'],
-                'payment_id'                                                 => ['required', 'integer'],
+                'payment_id'                                                 => ['required'],
                 'offer_id'                                                   => ['required', 'string'],
                 'passengers'                                                 => ['required', 'array', 'min:1'],
                 'passengers.*.id'                                            => ['required', 'string'],
